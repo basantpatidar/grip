1e4f2a893905f6d7f8c3f6183c446d6f5b248ad4
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2020-01-31 22:37:46 -0500

    overriding understanding

---
+++ src/gripOne/Car.java
@@ -0,0 +1,25 @@
+package gripOne;
+
+public class Car {
+	private int wheels;
+	private int doors;
+	private int mgp;
+	public int getWheels() {
+		return wheels;
+	}
+	public void setWheels(int wheels) {
+		this.wheels = wheels;
+	}
+	public int getDoors() {
+		return doors;
+	}
+	public void setDoors(int doors) {
+		this.doors = doors;
+	}
+	public int getMgp() {
+		return mgp;
+	}
+	public void setMgp(int mgp) {
+		this.mgp = mgp;
+	}
+}
