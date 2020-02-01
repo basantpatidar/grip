2d77e782ee0ba77e81e4051f92c03053786242e4
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2020-01-31 22:41:20 -0500

    overriding update

--- src/gripOne/Car.java
+++ src/gripOne/Car.java
@@ -4,6 +4,27 @@ public class Car {
 	private int wheels;
 	private int doors;
 	private int mgp;
+	private String make;
+	private String model;
+	private String color;
+	public String getMake() {
+		return make;
+	}
+	public void setMake(String make) {
+		this.make = make;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
 	public int getWheels() {
 		return wheels;
 	}
