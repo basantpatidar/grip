a43b86ea9e6fdf680dbdab35454cb98ae573b0bc
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2020-02-02 22:28:35 -0500

    object and class

---
+++ src/gripOne/Account.java
@@ -0,0 +1,51 @@
+package gripOne;
+
+public class Account {
+	
+	private int accountNumber;
+	private double balance;
+	private String customerName;
+	private String email;
+	private int phone;
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(int accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	
+	public double fundDeposit(double ammount) {
+		this.balance += ammount;
+		System.out.println(balance);
+		return balance;
+	}
+	public double fundWithdraw(double ammount) {
+		this.balance -= ammount;
+		System.out.println(balance);
+		return balance;
+	}
+}
---
+++ src/gripOne/AccountHoldler.java
@@ -0,0 +1,10 @@
+package gripOne;
+
+public class AccountHoldler {
+	public static void main(String[] args) {
+		Account at = new Account();
+		at.fundDeposit(100);
+		at.fundWithdraw(50);
+		
+	}
+}
