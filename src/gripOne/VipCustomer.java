8d03cbb5d222f9065c9006702bf5337ced0abce9
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2020-02-03 21:44:32 -0500

    under process

---
+++ src/gripOne/VipCustomer.java
@@ -0,0 +1,19 @@
+package gripOne;
+
+public class VipCustomer {
+	
+	private int creditLimit;
+	private String email;
+	
+	VipCustomer(){
+		VipCustomer();
+		
+	}
+	VipCustomer(){
+			
+		}	
+	VipCustomer(){
+		
+	}
+
+}
