f070b40d40cef5ca9e3dab6292621a10ab2d9014
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2020-02-07 22:19:48 -0500

    Account OOT

--- src/com/grip/one/AccountHoldler.java
+++ src/com/grip/one/AccountHoldler.java
@@ -1,10 +1,12 @@
-package gripOne;
+package com.grip.one;
 
 public class AccountHoldler {
 	public static void main(String[] args) {
 		Account at = new Account();
 		at.fundDeposit(100);
 		at.fundWithdraw(50);
+		
+		VipCustomer vc = new VipCustomer();
 		
 	}
 }
