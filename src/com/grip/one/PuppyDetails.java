6089089dbddf533e0f882e9fe7d6a5947f3be3fa
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2020-02-24 22:47:41 -0500

    puppyDetails

--- src/com/grip/one/PuppyDetails.java
+++ src/com/grip/one/PuppyDetails.java
@@ -1,10 +1,25 @@
-package gripOne;
+package com.grip.one;
 
-public class PuppyDetails{
+public class PuppyDetails implements Dog{
 	private String color;
 
 	public void setColor (String newColor){
 		this.color = newColor;
 	}
 	public String getColor(){
-		return this.color;}}
+		return this.color;}
+	@Override
+	public String breedName(String bName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public float height(float puppyHeight) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public float weight(float puppyWeight) {
+		// TODO Auto-generated method stub
+		return 0;
+	}}
