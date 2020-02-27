419d0702ae21a01cb5a172a97e5ded2306234fc3
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2020-02-26 22:49:48 -0500

    few methods

--- src/com/grip/collection/mapUse.java
+++ src/com/grip/collection/mapUse.java
@@ -8,16 +8,20 @@ public class mapUse {
 
 	public static void main(String[] args) {
 		Map <Integer, String> map = new HashMap <> ();
+		Map <Integer, String> mp = new HashMap <> ();
 		
 		map.put(01, "Java");
 		map.put(02, "JavaScript");
 		map.put(03, "Python");
 		map.put(04, "Php");
 		
+		mp.put(01, "React");
+		
 		System.out.println(map.containsKey(2));
 		System.out.println(map.containsValue("Java"));
 		
-//		map.equals()
+		
+		System.out.println(map.equals(mp));
 		map.clear();
 		
 		
