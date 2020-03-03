6d4aa0c4e8fc9665d0fd47c41fa7498d43c0e963
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2020-03-02 23:17:28 -0500

    colors

---
+++ src/com/grip/collection/Colors.java
@@ -0,0 +1,13 @@
+package com.grip.collection;
+import java.util.*;
+
+public class Colors {
+
+	public static void main(String args[]) {
+		List <String> lt = new ArrayList<>();
+		
+		lt.add("Blue");
+		lt.add("green");
+		lt.add("red);
+	}
+}
