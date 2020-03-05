8918b1b2fa2113b9892635f10dc78cf134627c7d
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2020-03-04 22:48:31 -0500

    type defined

--- src/com/grip/collection/First.java
+++ src/com/grip/collection/First.java
@@ -7,12 +7,12 @@ import java.util.Iterator;
 public class First {
 
 	public static void main(String[] args) {
-		Collection value = new ArrayList();
+		Collection <Integer> value = new ArrayList <>();
 		value.add(10);
 		value.add(20);
 		value.add(30);
 
-		Iterator it = value.iterator();
+		Iterator <Integer> it = value.iterator();
 
 		while (it.hasNext()) {
 			System.out.println(it.next());
