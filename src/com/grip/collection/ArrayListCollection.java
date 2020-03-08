d1dcda8ef7a1782e6f8babadafac6f2c59e52d34
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2020-03-08 13:39:30 -0400

    List replace element

--- src/com/grip/collection/ArrayListCollection.java
+++ src/com/grip/collection/ArrayListCollection.java
@@ -2,11 +2,25 @@ package com.grip.collection;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class ArrayListCollection {
 
 	public static void main(String args[]) {
-		ArrayList <String> list = new ArrayList<> (Arrays.asList("a","b","c"));
-		System.out.println(list.get(0));
+		List <String> list = new ArrayList<> (Arrays.asList("a","b","c"));
+//		Arrays.asList("a","b","c");
+		System.out.println(list);
+		
+		ArrayListCollection AC = new ArrayListCollection();
+		System.out.println(AC.replaceElement());
+		
+	}
+	
+	
+	
+	public List replaceElement() {
+		List <String> lt = new ArrayList<>(Arrays.asList("1","2","3","4"));
+		lt.set(2, "5");
+		return lt;
 	}
 }
