6e03d172a0d25d08395a8f08faba7401b1410fd4
basantpatidar <basantpatidar33@gmail.com>
2020-03-15 20:23:49 -0400

    collection list

---
+++ src/com/grip/collection/FirstPlaceListInsert.java
@@ -0,0 +1,28 @@
+package com.grip.collection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class FirstPlaceListInsert {
+	public static void main(String[] Args) {
+		List <String> lt = new ArrayList<>();
+		
+		lt.add("1");
+		lt.add("2");
+		lt.add("3");
+		
+		lt.add(0, "First Index");
+		
+		System.out.println(lt);
+	}
+}
+
+
+
+
+
+
+
+
