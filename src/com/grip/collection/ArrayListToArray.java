417c857a1545aa895f9b935f86aae14d9997f5c3
basantpatidar <basantpatidar33@gmail.com>
2020-12-23 13:15:34 -0500

    Adding arrayListToArray

---
+++ src/com/grip/collection/ArrayListToArray.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package com.grip.collection;
+
+import java.util.ArrayList;
+
+/**
+ * @author basant
+ *
+ */
+public class ArrayListToArray {
+
+	/**
+	 * 
+	 */
+	public ArrayListToArray() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<String>();
+
+		list.add("Python");
+
+		list.add("Java");
+
+		list.add("PHP");
+
+		list.add("C#");
+
+		list.add("C++");
+
+		list.add("Perl");
+
+		String[]  my_array = new String[list.size()];
+
+		list.toArray(my_array);
+
+		for (String  string : my_array)
+		{
+			System.out.println(string);
+		}
+	}
+}
